8b9b2de2de0877731e8333741d279adeef8ad96f
IchHasseMarvWirklichSehr <chrimo18@gmail.com>
2018-06-07 16:57:15 +0200

    lol

--- SWPInterpreter/src/main/scala/AST.scala
+++ SWPInterpreter/src/main/scala/AST.scala
@@ -3,22 +3,20 @@ sealed trait Node
 case class Program(functions: List[FunctionDeclaration], main: Node) extends Node
 case class FunctionDeclaration(name: String, params: List[String], body: Node) extends Node
 
-case class expr(block: List[block], recordDef: List[recordDef], recordAccess: recordAccess, varDec1: varDec1,
-                      varAssign: varAssign, cond: cond, call: call, list: list, variable: variable, int: int,
-                      bool: bool, braces: braces)
-
-case class block(expr: String)
-case class recordDef(recordValueDec1: String)
-case class recordAccess(recordRef: String, id: String)
-case class varDec1(id: String, expr: String)
-case class varAssign(id: String, expr: String)
-case class cond(expr: String, expr: String, expr: String)
-case class call(id:String, expr: List[String], expr: String)
-case class list(expr: List[String], expr: String)
-case class variable(id: String)
-case class int(int: String)
-case class bool(bool: String)
-case class braces(expr: String)
-
-
-ayayay
+//TODO Add case classes to represent the AST.
+case class Id(name: String) extends Node
+case class Variable(variable: Id) extends Node
+case class Int(i: Int) extends Node
+case class Bool(tf: String) extends Node
+case class Braces(main: Node) extends Node
+case class Block(param: List[Node]) extends Node
+case class VarDec1(name: Id, main: Node) extends Node
+case class VarAssign(name: Id, main: Node) extends Node
+case class Cond(head: Node, body: Node, tail: Node) extends Node
+case class Call(name: Id, params: List[String]) extends Node
+case class Liste(list: List[String]) extends Node
+case class RecordDef(main: List[RecordValueDec1]) extends Node
+case class RecordValueDec1(name: Id, main: Node) extends Node
+case class RecordAccess(param: RecordRef, name: Id) extends Node
+case class RecordRef(main : Node) extends Node
+case class Expr(main : Node) extends Node
--- SWPInterpreter/src/main/scala/Parser.scala
+++ SWPInterpreter/src/main/scala/Parser.scala
@@ -2,13 +2,33 @@ import scala.util.parsing.combinator._
 
 class ExpParser extends JavaTokenParsers {
 
+  def expr = block | recordDef | recordAccess | varDec1 | varAssign | cond | call | list | variable | int | bool | braces ^^ {_ => Expr(_)}
+
   // TODO Implement the expression parser and add additional parsers for terminal and non terminal symbols, where necessary.
-  def program: Parser[Program] = ???
-kjdnfjksdj00
-  def id = """(a-z)(A-Z|a-z|0-9|?|_)*"""
-  def int = """0 | (1-9)(0-9)* | -(1-9)(0-9)*"""
+  def program: Parser[Program] = rep(function ~ ";".r) ~ epxr
 
 
+
+
+
+
+
+  def function: Parser[FunctionDeclaration] = "fun".r ~ id ~ "(".r ~ rep1("[".r ~ rep(id ~ ",".r) ~ id) ~ ")".r ~ "=".r ~ expr
+  def braces = "(".r ~ expr ~ ")".r
+  def block = "{".r ~ rep(expr ~ ";".r) ~ "}".r
+  def varDec1 = "$".r ~ id ~ "=".r ~ expr
+  def varAssign = id ~ "=".r ~ expr
+  def cond = "if".r ~ expr ~ "then".r ~ expr ~ "else".r expr
+  def call = id ~ "(".r ~ rep1(rep(expr ~ ";".r) ~ expr) + ")".r
+  def list = "[".r ~ rep1(rep(expr ~ ",".r)~ expr) ~ "]".r
+  def variable = id
+  def recordDef = "object".r ~ "{".r ~ rep(recordValueDec1 ~ ";".r) ~ "}".r
+  def recordValueDec1 = "$".r ~ id ~ "=".r ~ expr
+  def recordRef = block | call | variable | recordDef | braces ^^ {_ => RecordRef(_)}
+  def recordAccess = recordRef ~ ".".r ~ id
+  def int = """0 | (1-9)(0-9)* | -(1-9)(0-9)*""".r ^^ {_.toInt}
+  def id = """(a-z)(A-Z | a-z | 0-9 | ? | _)*""".r ^^ {_to.String}
+  def bool = """True | False""".r
 }
 
 object ParseProgram extends ExpParser {

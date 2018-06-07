b1c4181147737a91855e8bc9cea1d188c86860f7
marvuni1 <marvin.rantschl@student.tugraz.at>
2018-06-07 17:18:59 +0200

    ichändertelol

--- SWPInterpreter/src/main/scala/AST.scala
+++ SWPInterpreter/src/main/scala/AST.scala
@@ -3,7 +3,11 @@ sealed trait Node
 case class Program(functions: List[FunctionDeclaration], main: Node) extends Node
 case class FunctionDeclaration(name: String, params: List[String], body: Node) extends Node
 
-//TODO Add case classes to represent the AST.
+// TODO Add case classes to represent the AST.
+
+case class RecordRef(main : Node) extends Node
+case class Expr(main : Node) extends Node
+
 case class Id(name: String) extends Node
 case class Variable(variable: Id) extends Node
 case class Int(i: Int) extends Node
@@ -18,5 +22,3 @@ case class Liste(list: List[String]) extends Node
 case class RecordDef(main: List[RecordValueDec1]) extends Node
 case class RecordValueDec1(name: Id, main: Node) extends Node
 case class RecordAccess(param: RecordRef, name: Id) extends Node
-case class RecordRef(main : Node) extends Node
-case class Expr(main : Node) extends Node
--- SWPInterpreter/src/main/scala/Parser.scala
+++ SWPInterpreter/src/main/scala/Parser.scala
@@ -2,33 +2,26 @@ import scala.util.parsing.combinator._
 
 class ExpParser extends JavaTokenParsers {
 
-  def expr = block | recordDef | recordAccess | varDec1 | varAssign | cond | call | list | variable | int | bool | braces ^^ {_ => Expr(_)}
-
   // TODO Implement the expression parser and add additional parsers for terminal and non terminal symbols, where necessary.
-  def program: Parser[Program] = rep(function ~ ";".r) ~ epxr
+  def program: Parser[Program] = rep(function ~ ";") ~ expr ^^ {case f ~ e => new Program(f, e)}
 
+  def function: Parser[FunctionDeclaration] = ("fun" ~> ident) ~ id ~ ("(" ~> arguments) ~ rep1(rep(id ~ ("," ~> arguments)) ~ id) ~ (")" ~> arguments) ~ ("=" ~> arguments) ~ expr ^^
 
-
-
-
-
-
-  def function: Parser[FunctionDeclaration] = "fun".r ~ id ~ "(".r ~ rep1("[".r ~ rep(id ~ ",".r) ~ id) ~ ")".r ~ "=".r ~ expr
-  def braces = "(".r ~ expr ~ ")".r
-  def block = "{".r ~ rep(expr ~ ";".r) ~ "}".r
-  def varDec1 = "$".r ~ id ~ "=".r ~ expr
-  def varAssign = id ~ "=".r ~ expr
-  def cond = "if".r ~ expr ~ "then".r ~ expr ~ "else".r expr
-  def call = id ~ "(".r ~ rep1(rep(expr ~ ";".r) ~ expr) + ")".r
-  def list = "[".r ~ rep1(rep(expr ~ ",".r)~ expr) ~ "]".r
+  def expr = block | recordDef | recordAccess | varDec1 | varAssign | cond | call | list | variable | int | bool | braces
+  def recordRef = block | call | variable | recordDef | braces
+  def id = """(a-z)(A-Z|a-z|0-9|?|_)""".r ^^ {_.toString }
+  def int = """0 | (1-9)(0-9)* | -(1-9)(0-9)""".r ^^ {_.toInt }
+  def block = "{" ~ expr ~ ")"
+  def varDec1 = "$" ~ id ~ "=" ~ expr
+  def varAssign = id ~ "=" ~ expr
+  def cond = "if" ~ expr ~ "then" ~ expr ~ "else" ~ expr
+  def call = id ~ "(" ~ rep1(rep(expr ~ ", ") ~ expr) ~ ")"
+  def list = "[" ~ rep1(rep(expr ~ ", ") ~ expr) ~ "]"
   def variable = id
-  def recordDef = "object".r ~ "{".r ~ rep(recordValueDec1 ~ ";".r) ~ "}".r
-  def recordValueDec1 = "$".r ~ id ~ "=".r ~ expr
-  def recordRef = block | call | variable | recordDef | braces ^^ {_ => RecordRef(_)}
-  def recordAccess = recordRef ~ ".".r ~ id
-  def int = """0 | (1-9)(0-9)* | -(1-9)(0-9)*""".r ^^ {_.toInt}
-  def id = """(a-z)(A-Z | a-z | 0-9 | ? | _)*""".r ^^ {_to.String}
-  def bool = """True | False""".r
+  def recordDef = "object" ~ "{" ~ rep(recordValueDec1 ~ ";") ~ "}"
+  def recordValueDec1 = "$" ~ id ~ "=" ~ expr
+  def recordAccess = recordRef ~ "." ~ id
+
 }
 
 object ParseProgram extends ExpParser {
